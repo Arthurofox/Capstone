e3f4155f4c6740f65a5fd5ba92833597351d4390
Kroalist <arthurofox@gmail.com>
2025-02-09 01:51:24 +0100

    error fixed again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["placeholder.com"], // Add any image domains you're using
-  },
 }
 
 module.exports = nextConfig
